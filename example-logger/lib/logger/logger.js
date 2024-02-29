635459bc58f8ac6838d662730224565dfdb74b7d
Serhii Saviuk <s.saviuk@wnb.com.ua>
2024-03-01 00:49:58 +0200

    Day 3 - added support for multiple arguments

--- example-logger/lib/logger/logger.js
+++ example-logger/lib/logger/logger.js
@@ -3,25 +3,31 @@ import {scoreLevel, level} from "./constants.js";
 import * as appenderStrategy from "./appenders/appenderStrategy.js"
 
 const logger = (category) => ({
-    info: (message) => {
+    trace: (...message) => {
+        executeLog(level.TRACE, category, message)
+    },
+    debug: (...message) => {
+        executeLog(level.DEBUG, category, message)
+    },
+    info: (...message) => {
         executeLog(level.INFO, category, message)
     },
-    warn: (message) => {
+    warn: (...message) => {
         executeLog(level.WARN, category, message)
     },
-    error: (message) => {
+    error: (...message) => {
         executeLog(level.ERROR, category, message)
     }
 });
 
 const appender = appenderStrategy.getAppender();
 
-function executeLog(level, category, message) {
+function executeLog(level, category, message = []) {
+    const data = message.join(" ");
     if (scoreLevel[level] <= config.scoreLevel) {
-        appender.log(Date.now(), level, category, message);
+        appender.log(Date.now(), level, category, data);
     }
 }
-
 
 
 export default {
